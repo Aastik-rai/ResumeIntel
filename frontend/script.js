cf3ba50bc4038b90d4fad4f1c57cf7a384f4df8b
Aastik-rai <aastik.25b01010279@abes.ac.in>
2026-09-04 20:38:18 +0530

    Connect frontend to deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -36,7 +36,7 @@ const navAnalyzeButton =
 // BACKEND URL
 // ===============================
 
-const API_URL = "http://127.0.0.1:8080";
+const API_URL = "https://resumeintel.onrender.com";
 
 
 // ===============================
